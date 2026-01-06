aa4321b4c114b3e1719aa765b32a545c877d6742
mostafaosman <mostafa.osman.fathi@gmail.com>
2026-01-07 01:10:49 +0200

    add: __init__ package

---
+++ RagSystem/__init__.py
@@ -0,0 +1,18 @@
+from .RagSystem import RagSystem
+
+from .llm_models import MistralLLM
+from .llm_models import Phi3LLM
+
+from .prompts import DefaultPrompt
+
+from .embade_functions import NomicEmbed
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "RagSystem",
+    "Phi3LLM",
+    "MistralLLM",
+    "DefaultPrompt",
+    "NomicEmbed",
+]

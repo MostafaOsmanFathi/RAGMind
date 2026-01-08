6b1628affacef8fb04d694282ba7c645bb9d0719
mostafaosman <mostafa.osman.fathi@gmail.com>
2026-01-08 17:15:35 +0200

    Fixing: env and main.py

--- RagService/main.py
+++ RagService/main.py
@@ -8,19 +8,22 @@ from RagSystem.workers_listener import start_listener
 def main():
     concurrent_raq_workers = 2
     concurrent_doc_workers = 5
-
     try:
-        if len(sys.argv) > 2:
+        if len(sys.argv) > 3:
             raise ValueError('incorrect number of arguments')
-        elif len(sys.argv) == 2:
+        elif len(sys.argv) == 3:
             concurrent_raq_workers = int(sys.argv[1])
             concurrent_doc_workers = int(sys.argv[2])
-        elif len(sys.argv) == 1:
+        elif len(sys.argv) == 2:
             concurrent_raq_workers = int(sys.argv[1])
-    except Exception as e:
-            print("invalid args, args is  <number_of_raq_workers> <number_of_doc_workers>")
 
-    start_listener(concurrent_raq_workers,concurrent_doc_workers)
+        start_listener(concurrent_raq_workers,concurrent_doc_workers)
+    except ValueError as e:
+            print("invalid args, args is  <number_of_raq_workers> <number_of_doc_workers>")
+            exit(1)
+    except Exception as e:
+        print("Error occured ", e)
+        exit(1)
 
 
 if __name__ == '__main__':

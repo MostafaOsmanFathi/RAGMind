cb06be63a5806021593560010c045b3596a3164d
mostafaosman <mostafa.osman.fathi@gmail.com>
2026-01-27 22:17:13 +0200

    Feat(routes, footer): adding main routes of the app and implement footer

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {Landing} from './pages/landing/landing';
+import {Login} from './pages/login/login';
+import {Register} from './pages/register/register';
+import {Collection} from './pages/collection/collection';
+import {Collections} from './pages/collections/collections';
+import {Settings} from './pages/settings/settings';
+import {authGuardGuard} from './guards/auth.guard-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Landing },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register},
+  { path: 'collections', component: Collections, canActivate: [authGuardGuard] },
+  { path: 'collection/:cid', component: Collection, canActivate: [authGuardGuard] },
+  { path: 'settings', component: Settings, canActivate: [authGuardGuard] },
+];

38c434ec50d394f717d0bdc5d3b17c3c25464c16
mostafaosman <mostafa.osman.fathi@gmail.com>
2026-02-03 03:16:04 +0200

    refactor: remove old architecture and keep langchain arch only

--- RagService/RagSystem/llm_models/Phi3LLM.py
+++
@@ -1,6 +0,0 @@
-from .OllamaLLM import OllamaLLM
-
-
-class Phi3LLM(OllamaLLM):
-    def __init__(self, ollama_options=None, ollama_url: str = None):
-        super().__init__('phi3', ollama_options, ollama_url)
